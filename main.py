c3550ee59b955196ce5c2312a5e09b0f1b1c7e6a
tjeaneric <ericjohn415@gmail.com>
2022-10-22 14:34:59 +0200

    [Feature] Incorporate a SQL database and implemented CRUD operations

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI
 from routes.users import user_router
 from routes.events import event_router
 import uvicorn
+from database.connection import conn
 
 
 app = FastAPI()
 # Register routes
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+
+@app.on_event("startup")
+def on_startup():
+    conn()
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
